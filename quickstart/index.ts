0f170c56b49a8e57a4d192bd71346d3c2a67a445
shinyay <shinya.com@gmail.com>
2023-06-06 15:06:36 +0900

    Add a process to listen port

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -1,1 +1,10 @@
+const server = Bun.serve({
+    fetch(req: Request): Response | Promise<Response> {
+        return new Response("Hello Bun!");
+      },
+    
+      // Optional port number - the default value is 3000
+      port: process.env.PORT || 3000,
+});
+
 console.log("Hello via Bun!");
